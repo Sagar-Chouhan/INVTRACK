2708cf0d86ac4731c93f4c7fd0a315bf4f0791f3
Sagar Chouhan <sagarchauhan08439@gmail.com>
2026-06-12 15:04:12 +0530

    feat: change ui to purple theme and optimize app load time

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import compression from 'compression'
 import { PORT } from './config.js'
 import { connectMongo } from './db.js'
 import authRoutes from './routes/auth.routes.js'
@@ -35,6 +36,7 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use(compression())
 app.use(morgan('dev'))
 app.use('/uploads', express.static('uploads'))
 
