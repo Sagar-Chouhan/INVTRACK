4f4ac4509c59d85cb1672ce229261d1f69529335
Sagar Chouhan <sagarchauhan08439@gmail.com>
2026-06-12 14:48:39 +0530

    chore: actually remove test files

--- backend/check-db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-const uri = 'mongodb+srv://sagarchauhan89230_db_user:7zfnM4bcWAlf6R7R@cluster0.czdwon2.mongodb.net/?appName=Cluster0';
-mongoose.connect(uri).then(() => {
-  console.log(mongoose.connection.db.databaseName);
-  process.exit(0);
-}).catch(console.error);
